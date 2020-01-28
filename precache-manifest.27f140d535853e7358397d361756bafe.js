22d25290fffdfae8032593c231cb8253d6b54af8
alvin22 <alvinwtf2000@gmail.com>
2020-01-28 01:18:56 +0100

    Updates

---
+++ precache-manifest.27f140d535853e7358397d361756bafe.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b84bd9710bcff1d6699a1eda3120771b",
+    "url": "/imalvin/index.html"
+  },
+  {
+    "revision": "1588ab52ee6494ea8144",
+    "url": "/imalvin/static/css/2.0559008f.chunk.css"
+  },
+  {
+    "revision": "5008df2f428c15adee30",
+    "url": "/imalvin/static/css/main.d0be8151.chunk.css"
+  },
+  {
+    "revision": "1588ab52ee6494ea8144",
+    "url": "/imalvin/static/js/2.fa6d7e7d.chunk.js"
+  },
+  {
+    "revision": "a0061a83d9d70252a73cf7df602028e8",
+    "url": "/imalvin/static/js/2.fa6d7e7d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5008df2f428c15adee30",
+    "url": "/imalvin/static/js/main.f31f2f4e.chunk.js"
+  },
+  {
+    "revision": "fd33a2f72b88266fbeae",
+    "url": "/imalvin/static/js/runtime-main.07483eea.js"
+  },
+  {
+    "revision": "732966db727847d12b6acc89fec30ac4",
+    "url": "/imalvin/static/media/alvin.732966db.png"
+  },
+  {
+    "revision": "64d093d04c073b253b24125285b0861e",
+    "url": "/imalvin/static/media/alvin_media.64d093d0.png"
+  },
+  {
+    "revision": "e3d17ae22329d9b106b82169829276cc",
+    "url": "/imalvin/static/media/alvin_media.e3d17ae2.mp4"
+  },
+  {
+    "revision": "c59f66ad2117ddebb2b630bed078f771",
+    "url": "/imalvin/static/media/collaboration.c59f66ad.svg"
+  },
+  {
+    "revision": "86016af75ac677b21c2a1d2a4fad509e",
+    "url": "/imalvin/static/media/deployment.86016af7.svg"
+  },
+  {
+    "revision": "77fb110cfe39caeef712a7348a23e974",
+    "url": "/imalvin/static/media/desktop.77fb110c.svg"
+  },
+  {
+    "revision": "3d937f7f0642eb139088c349ba6673bf",
+    "url": "/imalvin/static/media/ff15_boosting.3d937f7f.mp4"
+  },
+  {
+    "revision": "efe0b5d500f3578051f5019b095236b5",
+    "url": "/imalvin/static/media/ff_15_boosting.efe0b5d5.png"
+  },
+  {
+    "revision": "4ba69538efb2830a8fe502eaf615f6cc",
+    "url": "/imalvin/static/media/mobile.4ba69538.svg"
+  },
+  {
+    "revision": "eb9d79c982c258ab0a361d5099b1f8be",
+    "url": "/imalvin/static/media/planning.eb9d79c9.svg"
+  },
+  {
+    "revision": "0e0b089e57cf2eea3bcbbf5aff6a4a26",
+    "url": "/imalvin/static/media/poster.0e0b089e.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/imalvin/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/imalvin/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/imalvin/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/imalvin/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "ec075bbfe9e8a027c65054af8ae18872",
+    "url": "/imalvin/static/media/web.ec075bbf.svg"
+  }
+]);
